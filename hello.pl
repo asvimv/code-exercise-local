a3e4e2e193a25c692ec50a8827c5c2e868426821
asimsingh <asvimvsingh@yahoo.com>
2014-05-06 23:25:09 -0700

    First commit

--- hello.pl
+++ hello.pl
@@ -1,5 +1,13 @@
 #!/usr/bin/perl
+use lib 'c:\\Perl64\\lib';
 use strict ;
 use warnings;
-print "hello world in perl \n"
+use LWP::Simple;
+use JSON::PP;
+use Data::Dumper::Names;
+
+my $json_data = get(q[http://graph.facebook.com/pradeep.deepz]);
+
+print $json_data;
+
 
